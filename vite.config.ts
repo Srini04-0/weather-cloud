5f834c645ec841171cca47430f0366763c794704
Srini04-0 <sakthiseenu2004@gmail.com>
2025-11-06 20:42:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  server: {
+    host: true, // allows external access (needed for Render)
+    port: 8080,
+    allowedHosts: ["weather-cloud-l16f.onrender.com"], // ✅ your Render domain
+  },
+});
+
